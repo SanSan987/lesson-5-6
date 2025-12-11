e1719b4219a7041fc749bfe2c62cd407d0cc1c10
SanSan987 <155648403+SanSan987@users.noreply.github.com>
2025-12-11 02:14:58 +0200

    t3.micro in cpu_nodes ex t3.small

--- eks/main.tf
+++ eks/main.tf
@@ -22,7 +22,7 @@ module "eks" {
       max_size     = 5
       desired_size = 4
 
-      instance_types = ["t3.micro"]
+      instance_types = ["t3.small"]
       capacity_type  = "ON_DEMAND"
     }
 
